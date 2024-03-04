a90e1a38f10b546f075e48f90da4253bd8a16ebd
TokarevArtemV <tokarev.artem.v@gmail.com>
2024-03-04 23:33:28 +0200

    add validation to name and email

--- schemas/contactsSchemas.js
+++ schemas/contactsSchemas.js
@@ -1,8 +1,16 @@
 import Joi from "joi";
 
 const createContactSchema = Joi.object({
-  name: Joi.string().required(),
-  email: Joi.string().required(),
+  name: Joi.string()
+    .min(3)
+    .message("Name must have min 3 chr")
+    .max(30)
+    .message("Name must have max 30 chr")
+    .required(),
+  email: Joi.string()
+    .email({ minDomainSegments: 2, tlds: { allow: ["com", "net"] } })
+    .message("Must be a valid email")
+    .required(),
   phone: Joi.string()
     .pattern(/^\(\d{3}\) \d{3}-\d{4}$/)
     .message("Phone number must be like (000) 111-2233")
@@ -10,9 +18,17 @@ const createContactSchema = Joi.object({
 });
 
 const updateContactSchema = Joi.object({
-  name: Joi.string(),
-  email: Joi.string(),
-  phone: Joi.string(),
+  name: Joi.string()
+    .min(3)
+    .message("Name must have min 3 chr")
+    .max(30)
+    .message("Name must have max 30 chr"),
+  email: Joi.string()
+    .email({ minDomainSegments: 2, tlds: { allow: ["com", "net"] } })
+    .message("Must be a valid email"),
+  phone: Joi.string()
+    .pattern(/^\(\d{3}\) \d{3}-\d{4}$/)
+    .message("Phone number must be like (000) 111-2233"),
 })
   .min(1)
   .message("Body must have at least one field");
